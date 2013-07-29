3844d58433a8e9ac720e0bae0d7f71de1b88086a
wenqs27@gmail.com <wenqs27@gmail.com@c18526f7-c2e7-3838-cd50-f83b187c2ad4>
2013-07-29 14:24:16 +0000

    加入默认程序。

---
+++ trunk/etcss/src/main.java
@@ -0,0 +1,13 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Wings
+ * Date: 13-7-29
+ * Time: 下午10:19
+ * To change this template use File | Settings | File Templates.
+ */
+public class main {
+    public static void main(String [] args)
+    {
+        System.out.println("hello world.");
+    }
+}
