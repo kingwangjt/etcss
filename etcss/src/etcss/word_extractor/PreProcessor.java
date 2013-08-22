6c6f9967fc8fa3d80aec7e7eb596c1953e2cc964
wenqs27 <wenqs27@gmail.com>
2013-08-22 14:34:19 +0000

    加入了 etcss 模块.

---
+++ etcss/src/etcss/word_extractor/PreProcessor.java
@@ -0,0 +1,31 @@
+package etcss.word_extractor;
+
+import java.util.ArrayList;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Wings
+ * Date: 13-8-19
+ * Time: 上午11:39
+ */
+public class PreProcessor {
+    private ArrayList<String> sentencesList;
+
+    public PreProcessor(ArrayList<String> sentences) {
+        this.sentencesList = sentences;
+    }
+
+    public ArrayList<String> getSentences() {
+        return sentencesList;
+    }
+
+    public void setSentences(ArrayList<String> m_sentences) {
+        this.sentencesList = m_sentences;
+    }
+
+    public void process(PreProcessRule rule) {
+        for (String sentence : sentencesList) {
+
+        }
+    }
+}
